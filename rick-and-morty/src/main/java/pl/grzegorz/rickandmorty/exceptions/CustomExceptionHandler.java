3cd387f84ed45b869bc3017dbc4febdc80dba261
grzegorz_oladele <grzegorz.oladele@gmail.com>
2023-04-15 16:59:42 +0200

    rick-and-morty - createion exception handler

---
+++ rick-and-morty/src/main/java/pl/grzegorz/rickandmorty/exceptions/CustomExceptionHandler.java
@@ -0,0 +1,38 @@
+package pl.grzegorz.rickandmorty.exceptions;
+
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.time.LocalDateTime;
+
+import static org.springframework.http.HttpStatus.BAD_REQUEST;
+
+@RestControllerAdvice
+@RequiredArgsConstructor
+class CustomExceptionHandler {
+
+    private final HttpServletRequest request;
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    @ResponseStatus(BAD_REQUEST)
+    ErrorResponse handleIncorrectPageNumberError(IllegalArgumentException e) {
+        return getErrorResponse(e.getMessage(), BAD_REQUEST);
+    }
+
+    private ErrorResponse getErrorResponse(String message, HttpStatus status) {
+        return ErrorResponse.builder()
+                .withMessage(message)
+                .withResponseCode(status.value())
+                .withTimestamp(LocalDateTime.now().toString())
+                .withPath(getCurrentUrlPath())
+                .build();
+    }
+
+    private String getCurrentUrlPath() {
+        return request.getServletPath();
+    }
+}
---
+++ rick-and-morty/src/main/java/pl/grzegorz/rickandmorty/exceptions/ErrorResponse.java
@@ -0,0 +1,20 @@
+package pl.grzegorz.rickandmorty.exceptions;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+import static lombok.AccessLevel.PRIVATE;
+
+@Getter
+@Setter
+@AllArgsConstructor(access = PRIVATE)
+@Builder(setterPrefix = "with")
+class ErrorResponse {
+
+    private String message;
+    private String timestamp;
+    private int responseCode;
+    private String path;
+}
