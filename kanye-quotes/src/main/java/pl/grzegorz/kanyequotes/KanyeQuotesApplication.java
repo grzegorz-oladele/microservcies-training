2a61fb906ce3a6a72dc7fa956e94aa40df0eba77
grzegorz_oladele <grzegorz.oladele@gmail.com>
2023-04-15 14:24:58 +0200

    Creation 'kanye-quotes' project

---
+++ kanye-quotes/src/main/java/pl/grzegorz/kanyequotes/KanyeQuotesApplication.java
@@ -0,0 +1,13 @@
+package pl.grzegorz.kanyequotes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KanyeQuotesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KanyeQuotesApplication.class, args);
+    }
+
+}
---
+++ kanye-quotes/src/test/java/pl/grzegorz/kanyequotes/KanyeQuotesApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.grzegorz.kanyequotes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KanyeQuotesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
