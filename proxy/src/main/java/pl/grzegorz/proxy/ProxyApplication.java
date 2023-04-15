b9cdb2fbf11f265d683ad9e4a4c6d54801d484c4
grzegorz_oladele <grzegorz.oladele@gmail.com>
2023-04-15 17:11:40 +0200

    Creation proxy app

---
+++ proxy/src/main/java/pl/grzegorz/proxy/ProxyApplication.java
@@ -0,0 +1,13 @@
+package pl.grzegorz.proxy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProxyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProxyApplication.class, args);
+    }
+
+}
