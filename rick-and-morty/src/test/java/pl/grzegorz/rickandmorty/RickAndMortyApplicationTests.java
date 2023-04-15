7f6bb7c8ab46f262aa714d9ec7d5224a55de46d9
grzegorz_oladele <grzegorz.oladele@gmail.com>
2023-04-15 14:56:28 +0200

    Creation rick-and-morty project

---
+++ rick-and-morty/src/test/java/pl/grzegorz/rickandmorty/RickAndMortyApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.grzegorz.rickandmorty;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RickAndMortyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
